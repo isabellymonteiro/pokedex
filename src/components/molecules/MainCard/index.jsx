eba17a0d9ba257a1932b74b449131b7e1292a306
JrFogassi <jrfogassi@gmail.com>
2022-07-11 09:50:44 -0300

    fixing the box shadow from the cards + adding the link to the pokedex page in the database

--- src/components/molecules/MainCard/index.jsx
+++ src/components/molecules/MainCard/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import './style.scss';
 
-const MainCard = ({title, backgroundStyle}) => {
+const MainCard = ({title, backgroundStyle, linkTo}) => {
     return (
         <li className={`mainCard mainCard--${backgroundStyle}`}>
+            <Link to={linkTo} className="mainCard__link">
             <h2 className='mainCard__title'>{title}</h2>
+            </Link>
         </li>
     )
 }
