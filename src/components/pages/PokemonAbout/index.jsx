d18055ddcf7b501a970769b9a645bf47e5c2845e
JrFogassi <jrfogassi@gmail.com>
2022-07-13 13:47:01 -0300

    applying the fetch created in the PokemonAbout page

--- src/components/pages/PokemonAbout/index.jsx
+++ src/components/pages/PokemonAbout/index.jsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react'
+import { useCallback } from 'react'
+import { getPokemonAbout } from '../../../services/api'
 import './styles.scss'
 
 const PokemonAbout = () => {
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [pokemons, setPokemons] = useState([])
+
+  
+  const fetchPokemon = useCallback(async () => {
+    setLoading(true)
+    const pokemonData = await getPokemonAbout(bulbasaur)
+
+    if (pokemonData) {
+      setPokemons(pokemonData)
+    } else {
+      setError(true)
+    }
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    if (state) {
+      setPokemons(state)
+    } else {
+      fetchPokemon()
+    }
+  }, [fetchPokemon])
   return (
     <h2>Pokemon About</h2>
   )
