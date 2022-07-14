2e6e09b94d6dc66b7c259ce43a49cd799cb06e4d
JrFogassi <jrfogassi@gmail.com>
2022-07-14 10:16:32 -0300

    fetch pokemon about section data (more informations)

--- src/components/pages/PokemonAbout/index.jsx
+++ src/components/pages/PokemonAbout/index.jsx
@@ -2,41 +2,59 @@ import { useEffect, useCallback, useContext, useState } from 'react'
 import { getPokemonAbout } from '../../../services/api'
 import ErrorMessage from '@molecules/ErrorMessage'
 import LoadingSpinner from '@atoms/Icons/LoadingSpinner'
+import { useLocation, useParams } from 'react-router-dom'
 
 
 
 import './styles.scss'
 
 
+
 const PokemonAbout = () => {
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
-  const [pokemon, setPokemon] = useState([])
-  const {pokemonData} = useContext(PokemonContext)
-  
+  const [pokemon, setPokemon] = useState({})
+  const {state} = useLocation()
+  const {pokemonName} = useParams()
+
+  console.log(state)
+
   const fetchPokemon = useCallback(async () => {
     setLoading(true)
-    const additionalPokemonData = await getPokemonAbout(pokemonData.name)
+    const additionalPokemonData = await getPokemonAbout(pokemonName)
 
     if (additionalPokemonData) {
-      setPokemon(additionalPokemonData)
+
+      const pokemonEggGroups = additionalPokemonData.egg_groups.map((eggGroup) => {
+        return (
+          eggGroup.name
+        )
+      })
+      const pokemonDescription = additionalPokemonData.flavor_text_entries[6].flavor_text
+
+      const pokemonEggCycle = additionalPokemonData.hatch_counter
+
+      setPokemon({eggGroup: pokemonEggGroups, pokemonDescription: pokemonDescription, pokemonEggCycle: pokemonEggCycle})
     } else {
       setError(true)
     }
-    console.log(additionalPokemonData);
+
     setLoading(false)
   }, [])
 
   useEffect(() => {
     fetchPokemon()
   }, [fetchPokemon])
+
   return (
     <div>
         {error && <ErrorMessage />}
         {loading && <LoadingSpinner />}
-        {!!pokemon.length && pokemonData && !loading &&
+        {!loading && Object.keys(pokemon).length > 0 &&
         <>
-          
+          <span>
+            {pokemon.eggGroup}
+          </span>
         </>
       }
     </div>
