0940850bb4b6a959f39a2aa350de283534dc5cf6
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-07-15 12:20:55 -0300

    refactored about section fetch

--- src/components/pages/PokemonAbout/index.jsx
+++ src/components/pages/PokemonAbout/index.jsx
@@ -1,86 +1,77 @@
-import { useEffect, useCallback, useContext, useState } from 'react'
-import { getPokemonAbout } from '../../../services/api'
+import { useEffect, useCallback, useState } from 'react'
+import { getPokemonSpecies, getPokemon } from '../../../services/api'
 import ErrorMessage from '@molecules/ErrorMessage'
 import LoadingSpinner from '@atoms/Icons/LoadingSpinner'
 import { useLocation, useParams } from 'react-router-dom'
-
-
+import useFetchPokemon from '@hooks/useFetchPokemon'
 
 import './styles.scss'
 
-
-
 const PokemonAbout = () => {
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [errorSpecies, setErrorSpecies] = useState(false)
+  const [loadingSpecies, setLoadingSpecies] = useState(false)
   const [pokemon, setPokemon] = useState({})
-  const {state} = useLocation()
-  const {pokemonName} = useParams()
+  const { state } = useLocation()
+  const { pokemonName } = useParams()
 
-  console.log(state)
+  const { loading, error, pokemonData } = useFetchPokemon(pokemonName, getPokemon, state)
 
-  const fetchPokemon = useCallback(async () => {
-    setLoading(true)
-    const additionalPokemonData = await getPokemonAbout(pokemonName)
-
-
-    console.log(additionalPokemonData)
+  const fetchPokemonSpecies = useCallback(async () => {
+    setLoadingSpecies(true)
+    const additionalPokemonData = await getPokemonSpecies(pokemonName)
 
     if (additionalPokemonData) {
-
       const pokemonEggGroups = additionalPokemonData.egg_groups.map((eggGroup) => {
         return (
           eggGroup.name
         )
       })
 
-
       const pokemonDescription = additionalPokemonData.flavor_text_entries[6].flavor_text
-
       const pokemonEggCycle = additionalPokemonData.hatch_counter
 
-
-      setPokemon({eggGroup: pokemonEggGroups, 
-        pokemonDescription: pokemonDescription, 
+      setPokemon({
+        eggGroup: pokemonEggGroups,
+        pokemonDescription: pokemonDescription,
         pokemonEggCycle: pokemonEggCycle
-        })
+      })
     } else {
-      setError(true)
+      setErrorSpecies(true)
     }
 
-    setLoading(false)
+    setLoadingSpecies(false)
   }, [])
 
   useEffect(() => {
-    fetchPokemon()
-  }, [fetchPokemon])
+    fetchPokemonSpecies()
+  }, [fetchPokemonSpecies])
 
   return (
-    <div className='pokemonAboutSection'>
-        {error && <ErrorMessage />}
-        {loading && <LoadingSpinner />}
-        {!loading && state && Object.keys(pokemon).length > 0 &&
-        <>
+    <>
+      {error || errorSpecies && <ErrorMessage />}
+      {loading || loadingSpecies && <LoadingSpinner />}
+      {Object.keys(pokemonData).length > 0 && Object.keys(pokemon).length > 0 &&
+        <div className='pokemonAboutSection'>
           <p className='pokemonAboutSection_description'>
-            {pokemon.pokemonDescription}           
+            {pokemon.pokemonDescription}
           </p>
 
           <div className='pokemonAboutSection__sizeContainer'>
             <div className='pokemonAboutSection__size'>
               <span className='pokemonAboutSection__sizeName'>
-                Height 
+                Height
               </span>
               <span className='pokemonAboutSection__sizeNumber'>
-                {(state.height / 10).toString().padEnd(4, "0")} cm
+                {(pokemonData.height / 10).toString().padEnd(4, "0")} cm
               </span>
             </div>
 
             <div className='pokemonAboutSection__size'>
               <span className='pokemonAboutSection__sizeName'>
-                Weight 
+                Weight
               </span>
               <span className='pokemonAboutSection__sizeNumber'>
-                {state.weight / 10} kg
+                {pokemonData.weight / 10} kg
               </span>
             </div>
           </div>
@@ -92,29 +83,27 @@ const PokemonAbout = () => {
             <div className='pokemonAboutSection__breedingItems'>
               <div className='pokemonAboutSection__breedingItem'>
                 <span className='pokemonAboutSection__breedingName'>
-                Egg Groups
-                </span>   
+                  Egg Groups
+                </span>
                 <span className='pokemonAboutSection__breedingValue'>
-                <span className='pokemonAboutSection__breedingValue--1'>{pokemon.eggGroup[0]}</span> 
-                {pokemon.eggGroup[1] && <span className='pokemonAboutSection__breedingValue--2'>{pokemon.eggGroup[1]}</span>}
-                </span>   
+                  <span className='pokemonAboutSection__breedingValue--1'>{pokemon.eggGroup[0]}</span>
+                  {pokemon.eggGroup[1] && <span className='pokemonAboutSection__breedingValue--2'>{pokemon.eggGroup[1]}</span>}
+                </span>
               </div>
               <div className='pokemonAboutSection__breedingItem'>
                 <span className='pokemonAboutSection__breedingName'>
-                Egg Cycle
-                </span>   
+                  Egg Cycle
+                </span>
                 <span className='pokemonAboutSection__breedingValue'>
-                {pokemon.pokemonEggCycle}
-                </span>   
+                  {pokemon.pokemonEggCycle}
+                </span>
               </div>
 
             </div>
           </div>
-          
-        </>
-      }
-    </div>
-    
+        </div>
+      } 
+    </> 
   )
 }
 
