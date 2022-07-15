107d60ae37229dc0e6d18f6a2726066997251823
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-07-15 15:56:36 -0300

    added to be implemented text to moves section

--- src/components/pages/PokemonMoves/index.jsx
+++ src/components/pages/PokemonMoves/index.jsx
@@ -2,7 +2,7 @@ import './styles.scss'
 
 const PokemonMoves = () => {
   return (
-    <h2 style={{ margin-top: '20px' }}>To be implemented</h2>
+    <h2 className='pokemonMoves__title'>To be implemented...</h2>
   )
 }
 
